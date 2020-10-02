55fa0b9e70affea468ad5cdef68c788bd96914de
Roberto Antonio Quirpa Palma - Ceiba Software <roberto.quirpa@ceiba.com.co>
2020-10-02 09:38:25 -0500

    first commit

---
+++ src/main/java/co/com/ceiba/autocine/AutocineApplication.java
@@ -0,0 +1,13 @@
+package co.com.ceiba.autocine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutocineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutocineApplication.class, args);
+	}
+
+}
