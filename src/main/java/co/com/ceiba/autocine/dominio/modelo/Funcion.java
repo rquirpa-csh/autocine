c83d0b667f49417cafea54df5ed0879729ed73c4
roberto.quirpa <csh*736341>
2020-10-05 14:32:43 -0500

    Ajustes por analisis de sonar

--- src/main/java/co/com/ceiba/autocine/dominio/modelo/Funcion.java
+++ src/main/java/co/com/ceiba/autocine/dominio/modelo/Funcion.java
@@ -1,7 +1,14 @@
 package co.com.ceiba.autocine.dominio.modelo;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
+@AllArgsConstructor
 public class Funcion {
 
     private long id;
@@ -12,69 +19,4 @@ public class Funcion {
     private int capacidadDisponible;
     private double costo;
 
-    public Funcion(long id, String nombre, LocalDateTime fechaInicio, LocalDateTime fechaFin, int capacidadTotal, int capacidadDisponible, double costo) {
-        this.id = id;
-        this.nombre = nombre;
-        this.fechaInicio = fechaInicio;
-        this.fechaFin = fechaFin;
-        this.capacidadTotal = capacidadTotal;
-        this.capacidadDisponible = capacidadDisponible;
-        this.costo = costo;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public LocalDateTime getFechaInicio() {
-        return fechaInicio;
-    }
-
-    public void setFechaInicio(LocalDateTime fechaInicio) {
-        this.fechaInicio = fechaInicio;
-    }
-
-    public LocalDateTime getFechaFin() {
-        return fechaFin;
-    }
-
-    public void setFechaFin(LocalDateTime fechaFin) {
-        this.fechaFin = fechaFin;
-    }
-
-    public int getCapacidadTotal() {
-        return capacidadTotal;
-    }
-
-    public void setCapacidadTotal(int capacidadTotal) {
-        this.capacidadTotal = capacidadTotal;
-    }
-
-    public int getCapacidadDisponible() {
-        return capacidadDisponible;
-    }
-
-    public void setCapacidadDisponible(int capacidadDisponible) {
-        this.capacidadDisponible = capacidadDisponible;
-    }
-
-    public double getCosto() {
-        return costo;
-    }
-
-    public void setCosto(double costo) {
-        this.costo = costo;
-    }
 }
--- src/main/java/co/com/ceiba/autocine/infraestructura/persistencia/entidad/EntidadFuncion.java
+++ src/main/java/co/com/ceiba/autocine/infraestructura/persistencia/entidad/EntidadFuncion.java
@@ -1,9 +1,14 @@
 package co.com.ceiba.autocine.infraestructura.persistencia.entidad;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.Set;
 
+@Getter
+@Setter
 @Entity(name = "funciones")
 public class EntidadFuncion {
 
@@ -31,61 +36,5 @@ public class EntidadFuncion {
 
     @OneToMany(mappedBy = "funcion")
     private Set<EntidadFactura> facturas;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public LocalDateTime getFechaInicio() {
-        return fechaInicio;
-    }
-
-    public void setFechaInicio(LocalDateTime fechaInicio) {
-        this.fechaInicio = fechaInicio;
-    }
-
-    public LocalDateTime getFechaFin() {
-        return fechaFin;
-    }
-
-    public void setFechaFin(LocalDateTime fechaFin) {
-        this.fechaFin = fechaFin;
-    }
-
-    public int getCapacidadTotal() {
-        return capacidadTotal;
-    }
-
-    public void setCapacidadTotal(int capacidadTotal) {
-        this.capacidadTotal = capacidadTotal;
-    }
-
-    public int getCapacidadDisponible() {
-        return capacidadDisponible;
-    }
-
-    public void setCapacidadDisponible(int capacidadDisponible) {
-        this.capacidadDisponible = capacidadDisponible;
-    }
-
-    public double getCosto() {
-        return costo;
-    }
-
-    public void setCosto(double costo) {
-        this.costo = costo;
-    }
 
 }
